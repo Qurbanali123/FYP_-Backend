5fca778c8a9eeb63cb43c15a0b72e2366225a3fe
Qurban Ali <qurbanaliatish@gmail.com>
2025-12-16 11:59:46 +0500

    Add backend files

--- db.js
+++ db.js
@@ -1,16 +1,24 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT
+    ? parseInt(process.env.MYSQLPORT)
+    : process.env.DB_PORT
+    ? parseInt(process.env.DB_PORT)
+    : 3306,
+
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 export default db;
